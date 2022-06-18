a66cf6266fc4e4c89b919747a5decbe1900ab3db
Stephen Smith <stephenlehanesmith@gmail.com>
2022-06-17 20:23:32 -0700

    added rad_time function that will tell you if you will have a rad time

---
+++ radtimepredictor/radify.py
@@ -0,0 +1,20 @@
+from termcolor import colored
+
+def rad_time():
+    """Tell the computer what your plan is for the
+    day and it will tell you whether you will have
+    a rad time"""
+
+    string = input(colored("Tell me your plans for the day and I will tell you its raditude:", 'yellow'))
+    answer_decoded = string.split(" ")
+
+    rad_time_terms = ['code', 'coding', 'python', 'le wagon',
+                'coffee', 'cat']
+
+    for i in answer_decoded:
+        if i.lower() in rad_time_terms:
+            print(colored("Hell yeah, you're gonna have a rad time.", "green"))
+            return
+    else:
+        print(colored("Man, what a bummer, maybe, like, revise your priorities", "red"))
+        return
